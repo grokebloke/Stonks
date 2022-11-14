f0ab9c676dd841fa4c405aeb5a39073d7b3ece10
Jewel Benny <73890191+jewelben@users.noreply.github.com>
2022-11-14 18:19:32 +0530

    Add files via upload

---
+++ GoogleNewsScraper.py
@@ -0,0 +1,78 @@
+'''
+Scrape Google News using bs4
+'''
+from requests import get
+# from contextlib import closing
+from bs4 import BeautifulSoup
+import requests
+from lxml import html
+import pandas as pd
+import time
+import datetime as datetime
+import random
+
+timelist = pd.read_csv('techindicators_20.csv')['Date'][:]
+Ticker = 'Amazon Stock'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'}
+to_merge = pd.DataFrame({'Date': [],'Ticker': [], 'Url':[], 'headline': [], 'source': [], 'snippet': []})
+it = 0
+for date in timelist:
+    it += 1
+    #if it%100 != 0:
+    #    print('Fetching ',timelist[it])
+
+    time.sleep(abs(random.gauss(0,2)))
+
+    # Create URL
+    Date = str(date).replace("-","")
+    URL = 'https://www.google.com/search?q='+Ticker+'+'+Date+'&source=lnms&tbm=nws'
+
+    # Get URL
+    raw_html = requests.get(URL, headers=headers)
+    soup = BeautifulSoup(raw_html.text, 'lxml')
+    #title_list = soup.find_all("a", class_="l lLrAF")
+    #date_list = soup.find_all("span", class_="f nsa fwzPFf")
+    #source_list = soup.find_all("span", class_="xQ82C e8fRJf")
+    #snippet_list = soup.find_all("div", class_="st")
+    title_list = soup.find_all("a",class_="WlydOe")
+    #date_list = soup.find_all("div",class_="OSrXXb ZE0LJd YsWzw")
+    date_list = soup.find_all("div",class_="OSrXXb ZE0LJd YsWzw")
+    source_list = soup.find_all("div", class_="CEMjEf NUnG9d")
+    snippet_list = soup.find_all("div", class_="GI74Re nDgy9d")
+    #if it == 1:
+    #    print(date_list)
+
+
+    # Parse elements
+    title = []
+    date = []
+    source = []
+    snippet = []
+    url = []
+    for elem in title_list:
+        valid = str(elem).replace("<em>", "").replace("</em>", "")[19:-4]
+        out1 = valid.split("=")[2][1:-8]
+        out2 = valid.split("=")[-7].split("<")[0][23:-4]
+        url.append(out1)
+        title.append(out2)
+        #if it == 1:
+            #print(out1)
+            #print(out2)
+    for elem in date_list:
+        valid = str(elem)[58:-13]
+        date.append(valid)
+    for elem in source_list:
+        valid = str(elem)[311:-13]
+        source.append(valid)
+    for elem in snippet_list:
+        valid = str(elem).replace("<em>", "").replace("</em>", "")[71:-6]
+        snippet.append(valid)
+        #if it == 1:
+            #print(out1)
+            #print(valid)
+    if len(title) == len(date) and len(title) == len(url) and len(title) == len(source) and len(title) == len(snippet):
+        ticker = ['AMZ STOCK']*len(title)
+        news = pd.DataFrame({'Date':date,'Ticker': ticker,'Url': url, 'headline':title, 'source': source, 'snippet': snippet})
+        to_merge = pd.concat([to_merge,news])
+
+to_merge.to_csv('AMAZON_stock_related.csv')
